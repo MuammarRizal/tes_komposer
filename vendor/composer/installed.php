b91a4e027f679c4fea0bef59df5aa18b0a3cd78f
MuammarRizal <muammarrizal31@gmail.com>
2024-12-20 14:06:06 +0700

    tes komposer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'modernoop/pph21',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7261874c48c18d4435b0bda63b689ad95486d398',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'modernoop/pph21' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7261874c48c18d4435b0bda63b689ad95486d398',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
